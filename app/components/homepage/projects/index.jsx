05816b6c7e267029eeb1e0a2c026f05411a01c99
Prathamesh Patil <122prathamesh6002@sjcem.edu.in>
2025-06-28 23:32:07 +0530

    Did project to center

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -54,11 +54,11 @@ const Projects = () => {
   const currentEffect = effectOptions[effectType];
   
   return (
-    <div id="projects" className="relative z-50 my-12 lg:my-24">
+    <div id="projects" className="align-center items-center relative z-50 my-12 lg:my-24">
       <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
-        <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-1/2 transform -translate-x-1/2 filter blur-3xl  opacity-30"></div>
+        <div className="flex items-center justify-center relative">
+          <span className="bg-[#1a1443] absolute left-1/2 transform -translate-x-1/2 w-fit text-white px-5 py-3 text-xl rounded-md">
             PROJECTS
           </span>
           <span className="w-full h-[2px] bg-[#1a1443]"></span>
